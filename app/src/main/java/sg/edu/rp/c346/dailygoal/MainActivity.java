c752dcf48667f8dabd763d43621a07bb9a8b9fb8
KarenKuan <18055889@myrp.edu.sg>
2020-04-23 21:52:44 +0800

    Problem Statement - DailyGoal

--- app/src/main/java/sg/edu/rp/c346/dailygoal/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/dailygoal/MainActivity.java
@@ -2,7 +2,13 @@ package sg.edu.rp.c346.dailygoal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get the button and set the OnClickListener
+        Button btnOK = (Button) findViewById(R.id.buttonOK);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Get the RadioGroup object
+                RadioGroup rg1 = (RadioGroup) findViewById(R.id.radioGroup1);
+                RadioGroup rg2 = (RadioGroup) findViewById(R.id.radioGroup2);
+                RadioGroup rg3 = (RadioGroup) findViewById(R.id.radioGroup3);
+                // Get the comment that user keys in name
+                EditText etComment = (EditText) findViewById(R.id.editTextComment);
+                // Put the editText into an array
+                String[] info = {etComment.getText().toString()};
+                // Create an intent to start another activity called
+                // Summary (which we would create Later)
+                Intent i = new Intent(MainActivity.this, Summary.class);
+                // Pass the String array holding the comment to new activity
+                i.putExtra("info", info);
+                // Start the new activity
+                startActivity(i);
+            }
+        });
     }
 }
